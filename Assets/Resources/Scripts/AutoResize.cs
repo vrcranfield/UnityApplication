52ff7c7784f27838c7413c088e0954b4a1f3bbed
vrcranfield <ces.vr@cranfield.ac.uk>
2017-06-28 11:12:37 +0100

    Fixed autoresize.

--- Assets/Resources/Scripts/AutoResize.cs
+++ Assets/Resources/Scripts/AutoResize.cs
@@ -6,17 +6,29 @@ public class AutoResize : MonoBehaviour
 {
     [Tooltip("Activate or deactivate autoresizing")]
     public bool isAutoResizing = true;
-    float objectRadius;
+
+    [Tooltip("Set target radius")]
+    public float targetSize = 2;
 
     // Use this for initialization
     void Start () {
         if(isAutoResizing)
         {
-            float targetSize = 1;
+            MeshRenderer ren = GetComponentInChildren<MeshRenderer>();
 
-            //TODO fix
-            //objectRadius = GetComponent<Renderer>().bounds.size.magnitude;
-            //transform.localScale = targetSize * transform.localScale / objectRadius;
+            // Resize
+            float objectRadius = ren.bounds.size.magnitude;
+
+            Debug.Log("Object radius: " + objectRadius);
+            
+            transform.localScale = targetSize * transform.localScale / objectRadius;
+
+            // Recenter
+            /*Vector3 objectCenter = ren.bounds.center;
+
+            Debug.Log("Center: " + objectCenter.ToString());
+
+            transform.position -= new Vector3(objectCenter.x, 0, objectCenter.z);*/
         }
     }
 
