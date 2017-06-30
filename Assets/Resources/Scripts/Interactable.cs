e4b9f210e48758ffff84a5334148bbb8eb592676
Lorenzo <lorenzoderi314@gmail.com>
2017-06-30 17:45:19 +0100

    First attempt at fixing the collider for paraview objects

--- Assets/Resources/Scripts/Interactable.cs
+++ Assets/Resources/Scripts/Interactable.cs
@@ -19,5 +19,33 @@ public class Interactable : MonoBehaviour {
     {
         gameObject.AddComponent<BoxCollider>();
         gameObject.GetComponent<BoxCollider>().isTrigger = true;
+        FitColliderToChildren();
+    }
+
+    private void FitColliderToChildren()
+    {
+        bool hasBounds = false;
+        Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
+
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            Renderer childRenderer = transform.GetChild(i).GetComponent<MeshRenderer>() ;
+            if (childRenderer != null)
+            {
+                if (hasBounds)
+                {
+                    bounds.Encapsulate(childRenderer.bounds);
+                }
+                else
+                {
+                    bounds = childRenderer.bounds;
+                    hasBounds = true;
+                }
+            }
+        }
+
+        BoxCollider collider = GetComponent<BoxCollider>();
+        collider.center = bounds.center - transform.position;
+        collider.size = bounds.size;
     }
 }
